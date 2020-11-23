f48dc0f5784b3396b4ee4769d72e3eb8de72b703
Leonardo <leonardo.mdo90@gmail.com>
2020-11-23 00:53:01 +0000

    adicao do teste de integracao em categorias

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -12,5 +12,7 @@ class Genre extends Model
     protected $fillable = ['name','is_active'];
     protected $dates = ['deleted_at'];
     public    $incrementing = false;
-    protected $keyType = 'strings';
+    protected $casts = [
+        'id'=> 'string'
+    ];
 }
---
+++ tests/Unit/Models/CategoryTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Category;
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use PHPUnit\Framework\TestCase;
+
+
+class CategoryTest extends TestCase
+{
+    public function testFillableAttributes()
+    {
+        $category = new Category();
+        $this->assertEquals(['name','description','is_active'],$category->getFillable());
+    }
+
+    public function testDatesAttributes()
+    {
+        $dates = ['deleted_at', 'created_at','updated_at'];
+        $category = new Category();
+        foreach ($dates as $date){
+            $this->assertContains($date,$category->getDates());
+        }
+
+        $this->assertCount(count($dates),$category->getDates());
+    }
+
+    public function testIfUseTraits()
+    {
+        $traits = [
+            SoftDeletes::class, Uuid::class
+        ];
+
+        $categoryTraits = array_keys(class_uses(Category::class));
+
+        $this->assertEquals($traits,$categoryTraits);
+    }
+
+    public function testCasts()
+    {
+        $casts = ['id' => 'string', 'is_active' => 'boolean'];
+        $category = new Category();
+        $this->assertEquals($casts,$category->getCasts());
+    }
+
+    public function testIncrementing()
+    {
+        $category = new Category();
+        $this->assertFalse($category->incrementing);
+    }
+}
+
